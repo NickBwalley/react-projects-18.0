7875295bcbaadd377a60f8b74acf5a94433010ae
NickBwalley <nickbwalley@gmail.com>
2024-05-30 10:43:43 +0300

    accordion done

---
+++ 05-accordion/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Accordion from "./components/01-accordion";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        {/* Accordion component */}
+        <Accordion />
+      </div>
+    </>
+  );
+}
+
+export default App;
